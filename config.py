71167389d747db16bc43483c2980f38ffb8a939d
brimdor <chrisnelsonx@gmail.com>
2023-04-12 00:59:12 -0500

    adjusted columns in csv output, added sleep 1 sec

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-
+import time
 
 #CSV Output
 output_file = 'output/games.csv'
+fix_file = 'output/needs_fix.txt'
 
--- hltb.py
+++ hltb.py
@@ -32,20 +32,21 @@ def createcsv(games):
 def howlongtobeat(games):
     
     with open(output_file, 'w') as f:
-            f.write("title,game_name,game_alias,game_image_url,game_web_link,review_score,profile_dev,profile_platforms,release_world,main_story,main_extra,completionist,all_styles,\n")
+            f.write("game_name,game_alias,game_image_url,game_web_link,review_score,profile_dev,profile_platforms,release_world,main_story,main_extra,completionist,all_styles,user_submitted_name,\n")
     
     #games = ["Super Mario Bros. 3"]
     for game in games:
         results_list = HowLongToBeat().search(f'{game}', similarity_case_sensitive=False)
         if results_list is not None and len(results_list) > 0: 
-            results = f'"{game}","{results_list[0].game_name}","{results_list[0].game_alias}","{results_list[0].game_image_url}","{results_list[0].game_web_link}","{results_list[0].review_score}","{results_list[0].profile_dev}","{results_list[0].profile_platforms}","{results_list[0].release_world}","{results_list[0].main_story}","{results_list[0].main_extra}","{results_list[0].completionist}","{results_list[0].all_styles}",\n'
+            results = f'"{results_list[0].game_name}","{results_list[0].game_alias}","{results_list[0].game_image_url}","{results_list[0].game_web_link}","{results_list[0].review_score}","{results_list[0].profile_dev}","{results_list[0].profile_platforms}","{results_list[0].release_world}","{results_list[0].main_story}","{results_list[0].main_extra}","{results_list[0].completionist}","{results_list[0].all_styles}","{game}",\n'
             print(results)
             with open(output_file, 'a') as f:
                 f.write(results)
         else:
             print(f'{game},,,,,,,,,,,,,')
-            with open (output_file, 'a') as f:
-                f.write(f'"{game}",,,,,,,,,,,,,\n')
+            with open (fix_file, 'a') as f:
+                f.write(f'{game}\n')
+        time.sleep(1)
     
     return results
 
